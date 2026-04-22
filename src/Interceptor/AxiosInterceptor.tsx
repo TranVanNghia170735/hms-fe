0e7c2d68f6bb6a063c79c13215c12c7ab235ee76
NghiaTV3 <trannghi170735@gmail.com>
2026-04-22 16:59:17 +0700

    Update env

--- src/Interceptor/AxiosInterceptor.tsx
+++ src/Interceptor/AxiosInterceptor.tsx
@@ -1,7 +1,7 @@
 import axios, { InternalAxiosRequestConfig } from "axios";
 
 const axiosInstance = axios.create({
-   baseURL: "http://localhost:9000",
+   baseURL: process.env.REACT_APP_API_URL || "http://localhost:9000",
    headers: {
       "Content-Type": "application/json",
    },
